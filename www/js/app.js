4517fbc75f1cf8ab66c7f40d7615d4ac380c9cdd
Miguel Galindo <mgalindob@gmail.com>
2015-08-02 11:28:07 -0500

    Step 4

--- www/js/app.js
+++ www/js/app.js
@@ -1,42 +1,52 @@
 (function() {
-  var app = angular.module('myreddit', ['ionic', 'angularMoment'])
 
-  app.controller('ReditCtrl', function($http, $scope) {
+var app = angular.module('myreddit', ['ionic', 'angularMoment']);
 
-    $scope.stories = [];
+app.controller('RedditCtrl', function($http, $scope) {
 
-    $http.get('http://www.reddit.com/r/Android/new/.json')
+  $scope.stories = [];
+
+  function loadStories(params, callback) {
+    $http.get('http://www.reddit.com/r/Android/new/.json', {params: params})
       .success(function(response) {
-        console.log(response.data.children);
+        var stories = [];
         angular.forEach(response.data.children, function(child) {
-          $scope.stories.push(child.data);
+          stories.push(child.data);
         });
-
+        callback(stories);
       });
+  }
 
-    $scope.loadOlderStories = function() {
-      var params = {};
-      
-      $http.get('http://www.reddit.com/r/Android/new/.json', {params: params})
-        .success(function(response) {
-          console.log(response.data.children);
-          angular.forEach(response.data.children, function(child) {
-            $scope.stories.push(child.data);
-          });
-
-        });
-    };
-
-  });
-
-  app.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      if(window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
+  $scope.loadOlderStories = function() {
+    var params = {};
+    if ($scope.stories.length > 0) {
+      params['after'] = $scope.stories[$scope.stories.length - 1].name;
+    }
+    loadStories(params, function(olderStories) {
+      $scope.stories = $scope.stories.concat(olderStories);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
     });
-  })
-})();
+  };
+  
+  $scope.loadNewerStories = function() {
+    var params = {'before': $scope.stories[0].name};
+    loadStories(params, function(newerStories) {
+      $scope.stories = newerStories.concat($scope.stories);
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+}());
